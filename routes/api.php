a0075861bd7c9dd31327ebc88b52d5026e979ada
Abdulqayum <abdulqayumtoraboyevuz@gmail.com>
2024-10-07 22:03:16 +0500

    MINOR:Added seeder

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/tokens/yaratish',function () {
+    $user = User::query()->create([
+        'name' => 'Yarat',
+        'email' => 'yarat@gmail.com',
+        'password' => Hash::make('yaratish')
+    ]);
+    $token = $user->createToken('MyApp')->plainTextToken;
+    return response()->json(['token' => $token], 200);
+});
