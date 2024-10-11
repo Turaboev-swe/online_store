35ff4720a7375a4f81c94c5b26884e6758ea6b64
Abdulqayum <abdulqayumtoraboyevuz@gmail.com>
2024-10-11 17:28:02 +0500

    Patch:Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\CartController;
 use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\OrderController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\UserController;
 use App\Http\Resources\ProductResource;
@@ -16,7 +18,12 @@ Route::resource('/categories', CategoryController::class)
 Route::resource('/products', ProductController::class)
     ->middleware('auth:sanctum');
 
-//Route::get('category/{id}/products', [ProductController::class, 'showbycategory']);
+Route::resource('/cart', CartController::class)
+    ->middleware('auth:sanctum');
+
+Route::resource('/order', OrderController::class)
+    ->middleware('auth:sanctum');
+
 
 Route::get('/category/{id}/products', function (string $id) {
     return new ProductResource(Product::query()->findOrFail($id));
