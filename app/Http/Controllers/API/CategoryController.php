5cca0e040322ce58dff9fe2c65c60e4789dfbe6a
Abdulqayum <abdulqayumtoraboyevuz@gmail.com>
2024-10-17 14:39:53 +0500

    MINOR:Added test

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -1,60 +1,91 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index():JsonResponse
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $categories = Category::all();
-        return response()->json($categories);
-    }
-    public function show($id):JsonResponse
-    {
-        $categories = Category::query()->findOrFail($id);
-        return response()->json($categories);
+        return response()->json(Category::all());
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         //
     }
-    public function store(Request $request): JsonResponse
-    {
-        $category = Category::query()->create([
-            'name' => $request->get('name'),
-            'description' => $request->get('description'),
-            'parent_id' => $request->get('parent_id'),
-            'image' => $request->get('image')
-        ]);
-        return response()->json([
-            'message' => 'Categoriya yaratildi',
-            'product' => $category,
-        ], 201);
-    }
-    public function update(Request $request, string $id): JsonResponse
-    {
-        $category = Category::query()->findOrFail($id);
 
-        $category->update([
-            'name' => $request->input('name'),
-            'parent_id' => $request->input('parent_id'),
-            'description' => $request->input('description'),
-            'image' => $request->input('image')
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required'
         ]);
 
-        return response()->json($category);
-    }
-    public function destroy(string $id): JsonResponse
-    {
-        $category = Category::query()->findOrFail($id);
-        $category->delete();
+        $category = Category::create([
+            'name'      => $request->name,
+            'parent_id' => $request->parent_id
+        ]);
 
-        return response()->json(['message' => 'Category deleted successfully']);
+        return response()->json($category, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category): Category
+    {
+        return $category;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $updated = $category->update([
+            'name'      => $request->name,
+            'parent_id' => $request->parent_id
+        ]);
+
+        if ($updated) {
+            return response()->json(['name' => $category->name]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category):JsonResponse
+    {
+        $deleted = $category->delete();
+
+        if ($deleted) {
+            return response()->json([], 204);
+        }
+
+        return response()->json(['message' => 'Resource not deleted'], 500);
     }
 }
